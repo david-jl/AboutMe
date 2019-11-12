9ac13091010588f9ec1925c43b89d25f801b062e
David Jimenez <david.jimenez.lamas@alumnos.upm.es>
2019-11-12 14:59:20 +0100

    - Inicio nuevo diseño about me

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,10 @@
-var config = {
-    apiKey: "AIzaSyAsNZlUEfQ9Nq8GAMGrfshHUCE_FTqC-6E",
-    authDomain: "davidjl-aboutme.firebaseapp.com",
-    databaseURL: "https://davidjl-aboutme.firebaseio.com",
-    projectId: "davidjl-aboutme",
-    storageBucket: "davidjl-aboutme.appspot.com",
-    messagingSenderId: "734513722226"
-};
-firebase.initializeApp(config);
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+});
 
-// Reference messages collection
-var messagesRef = firebase.database().ref('messages');
+// Or with jQuery
 
-// Listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-// Submit form
-function submitForm(e){
-    e.preventDefault();
-    var nombre = getInputVal('nombre');
-    var email = getInputVal('email');
-    var asunto = getInputVal('asunto');
-    var mensaje = getInputVal('mensaje');
-
-    saveMessage(nombre, email, asunto, mensaje);
-
-    document.getElementById('contactForm').reset();
-    document.querySelector('.alerta').style.display = 'block';
-    document.querySelector('.alerta').style.animation = '1s alerta forwards';
-
-    setTimeout(function(){
-        document.querySelector('.alerta').style.animation = '1s alerta_quitar forwards';
-    },5000);
-    setTimeout(function(){
-        document.querySelector('.alerta').style.display = 'none';
-        document.querySelector('.footer_animation').style.animation = '1s subir_footer forwards';
-    },6000);
-
-    // Clear form
-
-}
-// Function to get get form values
-function getInputVal(id){
-    return document.getElementById(id).value;
-}
-
-// Save message to firebase
-function saveMessage(nombre, email, asunto, mensaje){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-        nombre: nombre,
-        email:email,
-        asunto:asunto,
-        mensaje:mensaje
-    });
-}
-
-//SG.cuSwEHP_Q3meJLTp3zStqg.4NhavSxFgNvmbCzRxuCWs9ueKAE0cZmh48BQm5GzcskSG.cuSwEHP_Q3meJLTp3zStqg.4NhavSxFgNvmbCzRxuCWs9ueKAE0cZmh48BQm5Gzcsk
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy();
+});
